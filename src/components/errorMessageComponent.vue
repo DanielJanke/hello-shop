c371686684aa097582fb9ea4ddc047b0f2253c0f
Daniel Janke <Daniel.Janke@me.com>
2017-12-01 15:55:11 +0100

    Removed in component css

--- src/components/errorMessageComponent.vue
+++ src/components/errorMessageComponent.vue
@@ -2,7 +2,7 @@
   <template>
     <div  v-bind:class="{visible: status.visible}" :style="{ 'background-color': status.type }" class="error">
       {{status.message}}
-        <div class="close" v-on:click="close">X</div>
+    <div class="close" v-on:click="close">X</div>
     </div>
   </template>
 
@@ -31,24 +31,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
-.close {
-  width: 10rem;
-}
-.error {
-  position: fixed;
-  width: 100%;
-  height: 0rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  background-color: red;
-  transition: 0.2s ease-in-out;
-  opacity: 0;
-}
-
-.visible {
-  height: 2rem;
-  opacity: 1;
-}
 </style>
